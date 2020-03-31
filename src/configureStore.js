f9a386134e244a724314c70b52f4dc00bc799d0e
Chris Arsenault <chrisstanarsenault@gmail.com>
2020-03-31 19:02:25 -0400

    setup guessWords tests

--- src/configureStore.js
+++ src/configureStore.js
@@ -2,8 +2,8 @@ import { createStore, applyMiddleware } from "redux";
 import ReduxThunk from "redux-thunk";
 import rootReducer from "./reducers";
 
-export const middleware = [ReduxThunk];
-export const createStoreWithMiddleware = applyMiddleware(...middleware)(
+export const middlewares = [ReduxThunk];
+export const createStoreWithMiddleware = applyMiddleware(...middlewares)(
   createStore
 );
 
